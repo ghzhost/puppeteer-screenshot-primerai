91451aa04279ad221f907d7a07d54c447eb3cf35
ghzhost <git@ghzhost.com>
2024-06-29 19:50:02 -0300

    Update puppeteer.ts

--- api/_lib/puppeteer.ts
+++ api/_lib/puppeteer.ts
@@ -19,8 +19,11 @@ export async function getScreenshot(url, width, height) {
   const page = await getPage();
   await page.setViewport({ width: Number(width) || 1280, height: Number(height) || 720, deviceScaleFactor: 2 });
   await page.goto(url);
+  await new Promise(r => setTimeout(r, 3000));
   const file = await page.screenshot();
   return file;
 }
+
+
 
 
