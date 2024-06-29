2e70175f26e808945b8e0efed11772305c03ff16
ghzhost <git@ghzhost.com>
2024-06-29 17:46:42 -0300

    Update puppeteer.ts

--- api/_lib/puppeteer.ts
+++ api/_lib/puppeteer.ts
@@ -5,10 +5,13 @@ let _page: Page | null;
 async function getPage() {
   if (_page) return _page;
   const options = {
-        args: chromium.args,
-        defaultViewport: chromium.defaultViewport,
-        executablePath: await chromium.executablePath(),
-        headless: chromium.headless,
+    args: [...chromium.args, '--hide-scrollbars', '--disable-web-security'],
+    defaultViewport: chromium.defaultViewport,
+    executablePath: await chromium.executablePath(
+      `https://github.com/Sparticuz/chromium/releases/download/v116.0.0/chromium-v116.0.0-pack.tar`
+    ),
+    headless: chromium.headless,
+    ignoreHTTPSErrors: true,
   };
   const browser = await launch(options);
   _page = await browser.newPage();
@@ -18,8 +21,12 @@ async function getPage() {
 export async function getScreenshot(url, width, height) {
   const page = await getPage();
   await page.setViewport({ width: Number(width) || 1280, height: Number(height) || 720, deviceScaleFactor: 2 });
-  await page.goto(url);
+  await page.goto(url, {
+    waitUntil: 'domcontentloaded',
+  });
   await new Promise(r => setTimeout(r, 5000));
   const file = await page.screenshot();
   return file;
 }
+
+
